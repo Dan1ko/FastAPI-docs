d2c896b5dfd21b4044cdf00f36eafa00f034e777
Dan1ko <daniko.sm@gmail.com>
2021-05-19 17:48:46 +0600

    file path

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from pydantic import BaseModel
 from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -13,14 +21,43 @@ class Item(BaseModel):
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
 
 
 @app.put("items/{item_id}")
 async def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item.id}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{files_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
